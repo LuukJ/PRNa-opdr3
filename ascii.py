2af45a3955c7e5d158df79ab48be913fb09a2dcd
levi223 <levi2234@hotmail.com>
2018-11-30 13:44:50 +0100

    Kruisjes toegevoegd op gezamelijke coordinaten

--- ascii.py
+++ ascii.py
@@ -1,18 +1,16 @@
 import numpy as np
 
-positionvec_1 = [[2.84,1.42],[1.42,0.72]]
-positionvec_2 = [[2.2,1.4],[0.4,0.1]]
-
+positionvec_1 = ([0.1,0.1],[2.48,1.42])
+positionvec_2 = [[0.1,0.1],[0.4,0.8]]
 
 def ascii_add (posarraybal):
     array = np.array(posarraybal)
-    x_a=  (array[:,0] / 2.84) * 18
-    y_a = (array[:, 1] / 1.42) * 70
+    x_a=  (array[:,0] / 2.48) * 70
+    y_a = (array[:, 1] / 1.42) * 18
     x_norm = np.round(x_a) - 1
     y_norm =np.round(y_a)
     m_stack = np.stack((x_norm, y_norm), axis=-1)
     return(m_stack)
-
 
 def ascii_plot_init (bal1,bal2):
     ascii_plot = []
@@ -26,14 +24,19 @@ def ascii_plot_init (bal1,bal2):
 
     for i in ascii_add(bal1):
         x = i.tolist()
-        ascii_plot[(18 -int(x[0])) * 72 + int(x[1])] = "o"
+        ascii_plot[(18 -int(x[1])) * 72 + int(x[0]) -1] = "o"
 
     for i in ascii_add(bal2):
         x = i.tolist()
-        ascii_plot[(18-int(x[0])) * 72 + int(x[1]) ] = "x"
 
+        if ascii_plot[(18-int(x[1])) * 72 + int(x[0])  -1] == "o":
+            ascii_plot[(18-int(x[1])) * 72 + int(x[0])  -1] = "+"
 
+        elif ascii_plot[(18-int(x[1])) * 72 + int(x[0])  -1] == " ":
+            ascii_plot[(18-int(x[1])) * 72 + int(x[0])  -1] = "x"
 
     print("".join(ascii_plot))
     print("+" +"-" * 70 + "+ ")
+
+
 ascii_plot_init(positionvec_1, positionvec_2)
